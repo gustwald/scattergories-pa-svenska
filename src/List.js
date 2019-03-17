3ce04314cee6971f4d403b171c5057f62c21f91c
Gustav <sundstrom-gustav@hotmail.com>
2019-03-18 00:18:31 +0100

    test

--- src/List.js
+++ src/List.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const List = ({ data }) => {
-    console.log('inuti filhelvetet');
     return (
   <ul>
-      <li>testshit</li>
       {data.map((d, i) => (
           <li key={i} className="list-item"><span className="number">{i + 1}.</span> {d.toUpperCase()}</li>
       ))}
